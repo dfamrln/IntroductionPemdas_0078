888d2c97f18ca5b6d1d40acb1eaf397570f2cdef
dfamrln <mhdffa1@gmail.com>
2026-02-23 11:15:32 +0700

    membuat program main

--- pemdas.cpp
+++ pemdas.cpp
@@ -1,2 +1,12 @@
-#include <iostream>
-using namespace std;
+#include <iostream> //file yang disertakan
+// using namespace std;
+using std::cout; 
+using std::endl;
+
+int main() // program utama
+{
+    // menampilakn string
+    cout << "selamat datang di TI UMY" << endl;
+    // menampilkan numeris
+    cout << 10;
+}
